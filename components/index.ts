213a678d8691afb8a4be2091e58abc2cb11357a9
anrypwnz <anrypwnz@gmail.com>
2021-09-21 00:41:17 +0300

    1

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export * from './Htag/Htag';
+export * from './Button/Button';
+export * from './Rating/Rating';
+export * from './Tag/Tag';
+export * from './Paragraph/Paragraph';
---
+++ layout/Layout.tsx
@@ -0,0 +1,35 @@
+import React, { FunctionComponent } from 'react';
+import { LayoutProps } from './Layout.props';
+import { Header } from './Header/Header';
+import { Sidebar } from './Sidebar/Sidebar';
+import { Footer } from './Footer/Footer';
+import { AppContextProvider, IAppContext } from '../context/app.context'
+import cn from 'classnames';
+import styles from './Layout.module.css';
+
+
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+    return (
+        <div className={styles.wrapper}>
+            <Header className={styles.header} />
+            <Sidebar className={styles.sidebar} />
+            <div className={styles.body}>
+                {children}
+            </div>
+            <Footer className={styles.footer} />
+        </div>
+    );
+};
+
+
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
+    return function withLayoutComponent(props: T): JSX.Element {
+        return (
+            <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+                <Layout>
+                    <Component {...props} />
+                </Layout>
+            </AppContextProvider>
+        );
+    };
+};
